fa5881dec85a0c53c840c6190344bff2e767f359
third1020 <34233131+third1020@users.noreply.github.com>
2020-07-27 21:50:26 +0700

    push

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 
 import registerServiceWorker from './registerServiceWorker';
-window.$URLbackend = "http://localhost:3000";
+window.$URLbackend = "http://52.74.208.126:3000";
 window.$URLfontend = "http://localhost:3001";
 
 
